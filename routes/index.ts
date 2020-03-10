40b1fd2b1238a2b66bb6522779d56a77b0a79d0e
Inunor <bakatovic@gmail.com>
2020-03-10 21:06:26 +0300

    add request with load status

--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,26 @@
 import express from 'express';
+
+import { Status } from './interfaces';
 import FilmsScraper from '../src/tutby/films';
+import { Film } from '../src/tutby/films/interfaces';
 
 const router = express.Router();
 
+let filmsStatus: Status = Status.Start;
+let films: Film[] = [];
 router.get('/films', async (request, response) => {
-  const scraper = new FilmsScraper();
-  const data = await scraper.getData();
-  response.json(data);
+  if (filmsStatus === Status.Start) {
+    const scraper = new FilmsScraper();
+    response.sendStatus(202);
+    filmsStatus = Status.Progress;
+    films = await scraper.getData();
+    filmsStatus = Status.Finish;
+  } else if (filmsStatus === Status.Progress) {
+    response.sendStatus(202);
+  } else if (filmsStatus === Status.Finish) {
+    filmsStatus = Status.Start;
+    response.json(films);
+  }
 });
 
 export default router;
