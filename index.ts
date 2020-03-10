ea00766d71f7fecf8a56dde6dda813c0ae7cfdcc
Inunor <bakatovic@gmail.com>
2020-03-10 19:43:20 +0300

    add start endpoint

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.end('<h1>Placard scraper</h1>');
+});
+
 app.use('/tutby', data);
 
 const port = process.env.PORT || 5050;
