abc69d146d9494703310b7ecbe0255bc5e0a4f0a
Inunor <bakatovic@gmail.com>
2020-05-10 16:00:23 +0300

    add films from relaxby (+ small refactoring)

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import data from './routes';
+
+import tutByData from './routes/tutby';
+import relaxByData from './routes/relaxby';
 
 const app = express();
 
@@ -10,7 +12,8 @@ app.get('/', (req, res) => {
   res.end('<h1>Placard scraper</h1>');
 });
 
-app.use('/tutby', data);
+app.use('/tutby', tutByData);
+app.use('/relaxby', relaxByData);
 
 const port = process.env.PORT || 8080;
 
